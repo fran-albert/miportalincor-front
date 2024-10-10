71ddd1c39041225e9a8b5f65308b51ac84105ec5
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-10-09 22:13:49 -0300

    no message

--- src/components/Button/Logout/index.tsx
+++ src/components/Button/Logout/index.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
 import { logout } from "@/store/authSlice";
+import LoadingAnimation from "@/components/Loading/loading";
 
 const LogoutButton = () => {
   const dispatch = useDispatch();
@@ -11,24 +12,27 @@ const LogoutButton = () => {
   const handleLogout = () => {
     setIsLoggingOut(true);
 
-    localStorage.removeItem("authToken");
-    dispatch(logout());
     setTimeout(() => {
+      localStorage.removeItem("authToken");
+      dispatch(logout());
       setIsLoggingOut(false);
-      navigate("/iniciar-sesion"); 
-    }, 1000);
+      navigate("/iniciar-sesion");
+    }, 2000);
   };
 
   return (
-    <button
-      onClick={handleLogout}
-      className={`group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-bold transition-colors text-white hover:bg-red-200 hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 ${
-        isLoggingOut ? "opacity-50 cursor-not-allowed" : ""
-      }`}
-      disabled={isLoggingOut}
-    >
-      {isLoggingOut ? "Saliendo..." : "Salir"}
-    </button>
+    <div className="flex justify-center items-center">
+      {isLoggingOut ? (
+        <LoadingAnimation />
+      ) : (
+        <button
+          onClick={handleLogout}
+          className={`group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-bold transition-colors text-white hover:bg-red-200 hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50`}
+        >
+          Salir
+        </button>
+      )}
+    </div>
   );
 };
 
