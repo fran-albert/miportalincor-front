6ad90f3d7999aa34aa943f23bffc630219d09806
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2025-01-10 10:35:59 -0300

    no message

--- src/api/Patient/get-all-patients.action.ts
+++ src/api/Patient/get-all-patients.action.ts
@@ -2,14 +2,14 @@ import { sleep, slugify } from "@/common/helpers/helpers";
 import { Patient } from "@/types/Patient/Patient";
 import axiosInstance from "@/services/axiosConfig";
 
-export const getPatients = async (page?: number): Promise<Patient[]> => {
+export const getPatients = async (): Promise<Patient[]> => {
     await sleep(2);
 
-    const params = new URLSearchParams();
-    params.append('page', `${page}`);
-    params.append('per_page', '5');
+    // const params = new URLSearchParams();
+    // params.append('page', `${page}`);
+    // params.append('per_page', '5');
 
-    const { data } = await axiosInstance.get<Patient[]>(`Patient/all`, { params });
+    const { data } = await axiosInstance.get<Patient[]>(`Patient/all`);
     const patientsWithSlugs = data.map(patient => ({
         ...patient,
         slug: slugify(`${patient.firstName} ${patient.lastName}`, patient.userId)
--- src/hooks/useRoles.ts
+++ src/hooks/useRoles.ts
@@ -1,5 +1,7 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { jwtDecode } from 'jwt-decode';
+import { logout } from '@/store/authSlice';
+import { useEffect } from 'react';
 
 interface DecodedToken {
   Id: string;
@@ -18,7 +20,37 @@ const ROLES = {
 };
 
 const useUserRole = () => {
-  const token = useSelector((state: any) => state.auth.token) || (typeof window !== 'undefined' ? localStorage.getItem('authToken') : null);
+  const dispatch = useDispatch();
+  const token = useSelector((state: any) => state.auth.token);
+  const tokenExpiration = useSelector((state: any) => state.auth.tokenExpiration);
+
+  useEffect(() => {
+    if (!token || !tokenExpiration) return;
+
+    const checkAndHandleExpiration = () => {
+      const currentTime = Date.now();
+      const expirationTime = parseInt(tokenExpiration);
+
+      if (currentTime >= expirationTime) {
+        dispatch(logout());
+      }
+    };
+
+    // Verificar inmediatamente
+    checkAndHandleExpiration();
+
+    // Calcular el tiempo restante hasta la expiración
+    const timeUntilExpiration = parseInt(tokenExpiration) - Date.now();
+    if (timeUntilExpiration <= 0) return;
+
+    // Configurar el temporizador para el logout
+    const logoutTimer = setTimeout(() => {
+      dispatch(logout());
+    }, timeUntilExpiration);
+
+    // Limpiar el temporizador cuando el componente se desmonte
+    return () => clearTimeout(logoutTimer);
+  }, [token, tokenExpiration, dispatch]);
 
   if (!token) {
     return {
@@ -35,6 +67,7 @@ const useUserRole = () => {
     decodedToken = jwtDecode(token) as DecodedToken;
   } catch (error) {
     console.error('Error decodificando el token', error);
+    dispatch(logout());
     return {
       isPatient: false,
       isDoctor: false,
